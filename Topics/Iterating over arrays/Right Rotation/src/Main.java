43bf8143e9bd70e24620ac75855f9c18e1e0f3e2
Falcon,Emanuel <Emanuel.Falcon@Cerner.com>
2021-07-27 16:21:49 -0500

    Exercises done on July 27, 2021

--- Topics/Iterating over arrays/Right Rotation/src/Main.java
+++ Topics/Iterating over arrays/Right Rotation/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        String[] array = scanner.nextLine().split(" ");
+
+        int shift = scanner.nextInt();
+        shift %= array.length;
+
+        String last;
+
+        for (int x = 0; x < shift; x++) {
+            last = array[array.length - 1];
+            System.arraycopy(array, 0, array, 1, array.length - 1);
+            array[0] = last;
+        }
+
+        for (String s : array) {
+            System.out.print(s + " ");
+        }
+
     }
+
 }
--- Topics/Iterating over arrays/The index of the first max in an array/src/Main.java
+++ Topics/Iterating over arrays/The index of the first max in an array/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int elements = scanner.nextInt();
+        int[] array = new int[elements];
+
+        for (int i = 0; i < elements; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = 0;
+        int index = 0;
+
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j;
+            }
+        }
+
+        System.out.println(index);
     }
 }
